8238742d28e086a4800ecaf6c4b18e84878b96ae
mdansar03 <mohammed.ansar@kaaylabs.com>
2024-05-16 19:03:25 +0530

    Fixes

--- scripts/services.js
+++ scripts/services.js
@@ -45,26 +45,26 @@ button[1].addEventListener('mouseleave', () => {
 
 const enterprise_tag = document.getElementById('enterprise_tag');
 const business_tag = document.getElementById('business_tag');
-const software_tag = document.getElementById('software_tag');
+// const software_tag = document.getElementById('software_tag');
 const cloud_tag = document.getElementById('cloud_tag');
 const machine_tag = document.getElementById('machine_tag');
 
 
 const active_enterprise_tag = document.getElementById('active_enterprise_tag');
 const active_business_tag = document.getElementById('active_business_tag');
-const active_software_tag = document.getElementById('active_software_tag');
+// const active_software_tag = document.getElementById('active_software_tag');
 const active_cloud_tag = document.getElementById('active_cloud_tag');
 const active_machine_tag = document.getElementById('active_machine_tag');
 
 const enterprise_text_redirect = document.getElementById('enterprise_text_redirect');
 const business_text_redirect = document.getElementById('business_text_redirect');
-const software_text_redirect = document.getElementById('software_text_redirect');
+// const software_text_redirect = document.getElementById('software_text_redirect');
 const cloud_text_redirect = document.getElementById('cloud_text_redirect');
 const machine_text_redirect = document.getElementById('machine_text_redirect');
 
 const enterprise_text_svg = document.getElementById('enterprise_text_svg');
 const business_text_svg = document.getElementById('business_text_svg');
-const software_text_svg = document.getElementById('software_text_svg');
+// const software_text_svg = document.getElementById('software_text_svg');
 const cloud_text_svg = document.getElementById('cloud_text_svg');
 const machine_text_svg = document.getElementById('machine_text_svg');
 
@@ -75,8 +75,8 @@ enterprise_tag.addEventListener('mouseenter', ()=>
     enterprise_text_svg.classList.add('enterprise_text_svg_active');
     business_text_svg.classList.remove('business_text_svg_active');
     business_text_redirect.classList.remove('business_text_redirect_active');
-    software_text_redirect.classList.remove('software_text_redirect_active');
-    software_text_svg.classList.remove('software_text_svg_active');
+    // software_text_redirect.classList.remove('software_text_redirect_active');
+    // software_text_svg.classList.remove('software_text_svg_active');
     cloud_text_redirect.classList.remove('cloud_text_redirect_active');
     cloud_text_svg.classList.remove('cloud_text_svg_active');
     machine_text_redirect.classList.remove('machine_text_redirect_active');
@@ -85,7 +85,7 @@ enterprise_tag.addEventListener('mouseenter', ()=>
 
     active_enterprise_tag.classList.add('active_enterprise_tag');
     active_business_tag.classList.remove('active_business_tag');
-    active_software_tag.classList.remove('active_software_tag');
+    // active_software_tag.classList.remove('active_software_tag');
     active_cloud_tag.classList.remove('active_cloud_tag');
     active_machine_tag.classList.remove('active_machine_tag');
 })
@@ -96,8 +96,8 @@ business_tag.addEventListener('mouseenter', ()=>
 
     enterprise_text_redirect.classList.remove('enterprise_text_redirect_active');
     enterprise_text_svg.classList.remove('enterprise_text_svg_active');
-    software_text_redirect.classList.remove('software_text_redirect_active');
-    software_text_svg.classList.remove('software_text_svg_active');
+    // software_text_redirect.classList.remove('software_text_redirect_active');
+    // software_text_svg.classList.remove('software_text_svg_active');
     cloud_text_redirect.classList.remove('cloud_text_redirect_active');
     cloud_text_svg.classList.remove('cloud_text_svg_active');
     machine_text_redirect.classList.remove('machine_text_redirect_active');
@@ -106,33 +106,33 @@ business_tag.addEventListener('mouseenter', ()=>
 
     active_business_tag.classList.add('active_business_tag');
     active_enterprise_tag.classList.remove('active_enterprise_tag')
-    active_software_tag.classList.remove('active_software_tag');
+    // active_software_tag.classList.remove('active_software_tag');
     active_cloud_tag.classList.remove('active_cloud_tag');
     active_machine_tag.classList.remove('active_machine_tag');
 
 })
-software_tag.addEventListener('mouseenter', ()=>
-{
-    software_text_redirect.classList.add('software_text_redirect_active');
-    software_text_svg.classList.add('software_text_svg_active');
+// software_tag.addEventListener('mouseenter', ()=>
+// {
+//     software_text_redirect.classList.add('software_text_redirect_active');
+//     software_text_svg.classList.add('software_text_svg_active');
 
-    enterprise_text_redirect.classList.remove('enterprise_text_redirect_active');
-    enterprise_text_svg.classList.remove('enterprise_text_svg_active');
-    business_text_svg.classList.remove('business_text_svg_active');
-    business_text_redirect.classList.remove('business_text_redirect_active');
-    cloud_text_redirect.classList.remove('cloud_text_redirect_active');
-    cloud_text_svg.classList.remove('cloud_text_svg_active');
-    machine_text_redirect.classList.remove('machine_text_redirect_active');
-    machine_text_svg.classList.remove('machine_text_svg_active');
+//     enterprise_text_redirect.classList.remove('enterprise_text_redirect_active');
+//     enterprise_text_svg.classList.remove('enterprise_text_svg_active');
+//     business_text_svg.classList.remove('business_text_svg_active');
+//     business_text_redirect.classList.remove('business_text_redirect_active');
+//     cloud_text_redirect.classList.remove('cloud_text_redirect_active');
+//     cloud_text_svg.classList.remove('cloud_text_svg_active');
+//     machine_text_redirect.classList.remove('machine_text_redirect_active');
+//     machine_text_svg.classList.remove('machine_text_svg_active');
 
 
-    active_software_tag.classList.add('active_software_tag');
-    active_enterprise_tag.classList.remove('active_enterprise_tag');
-    active_business_tag.classList.remove('active_business_tag');
-    active_cloud_tag.classList.remove('active_cloud_tag');
-    active_machine_tag.classList.remove('active_machine_tag');
+//     // active_software_tag.classList.add('active_software_tag');
+//     active_enterprise_tag.classList.remove('active_enterprise_tag');
+//     active_business_tag.classList.remove('active_business_tag');
+//     active_cloud_tag.classList.remove('active_cloud_tag');
+//     active_machine_tag.classList.remove('active_machine_tag');
 
-})
+// })
 cloud_tag.addEventListener('mouseenter', ()=>
 {
     cloud_text_redirect.classList.add('cloud_text_redirect_active');
@@ -142,8 +142,8 @@ cloud_tag.addEventListener('mouseenter', ()=>
     enterprise_text_svg.classList.remove('enterprise_text_svg_active');
     business_text_svg.classList.remove('business_text_svg_active');
     business_text_redirect.classList.remove('business_text_redirect_active');
-    software_text_redirect.classList.remove('software_text_redirect_active');
-    software_text_svg.classList.remove('software_text_svg_active');
+    // software_text_redirect.classList.remove('software_text_redirect_active');
+    // software_text_svg.classList.remove('software_text_svg_active');
     machine_text_redirect.classList.remove('machine_text_redirect_active');
     machine_text_svg.classList.remove('machine_text_svg_active');
 
@@ -151,7 +151,7 @@ cloud_tag.addEventListener('mouseenter', ()=>
 
     active_cloud_tag.classList.add('active_cloud_tag');
     active_enterprise_tag.classList.remove('active_enterprise_tag');
-    active_software_tag.classList.remove('active_software_tag');
+    // active_software_tag.classList.remove('active_software_tag');
     active_business_tag.classList.remove('active_business_tag');
     active_machine_tag.classList.remove('active_machine_tag');
 
@@ -165,8 +165,8 @@ machine_tag.addEventListener('mouseenter', ()=>
     enterprise_text_svg.classList.remove('enterprise_text_svg_active');
     business_text_svg.classList.remove('business_text_svg_active');
     business_text_redirect.classList.remove('business_text_redirect_active');
-    software_text_redirect.classList.remove('software_text_redirect_active');
-    software_text_svg.classList.remove('software_text_svg_active');
+    // software_text_redirect.classList.remove('software_text_redirect_active');
+    // software_text_svg.classList.remove('software_text_svg_active');
     cloud_text_redirect.classList.remove('cloud_text_redirect_active');
     cloud_text_svg.classList.remove('cloud_text_svg_active');
 
@@ -174,18 +174,6 @@ machine_tag.addEventListener('mouseenter', ()=>
     active_machine_tag.classList.add('active_machine_tag');
     active_enterprise_tag.classList.remove('active_enterprise_tag');
     active_cloud_tag.classList.remove('active_cloud_tag');
-    active_software_tag.classList.remove('active_software_tag');
-    active_business_tag.classList.remove('active_business_tag');
+    // active_software_tag.classList.remove('active_software_tag/');
+    // active_business_tag.classList.remove('active_business_tag');
 })
-
-
-// const service_text_redirect = document.getElementById('service_text_redirect');
-// const service_text_svg = document.getElementById('service_text_svg');
-
-
-
-// enterprise_tag.addEventListener('mouseenter', ()=>
-// {
-//     service_text_redirect.classList.add('service_text_redirect_active');
-//     service_text_svg.classList.add('service_text_svg_active');
-// })
